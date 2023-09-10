be7e60c499abe01f41ab533261d1a21500548111
MutabPato <patrickmtbz@gmail.com>
2023-09-10 12:53:45 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - is the main function
+ *
+ *Return: returns 0 ends program.
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+if (n < '9')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
