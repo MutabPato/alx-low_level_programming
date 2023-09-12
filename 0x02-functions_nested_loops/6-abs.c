10d5ff0141a22e5afd719b1c0408a1db9a2fa8c9
MutabPato <patrickmtbz@gmail.com>
2023-09-12 13:52:53 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_abs -  gives a number's absolute.
+ *@n: parameter
+ *Return: ends program.
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-1 * n);
+}
+}
