dfa4c8de4be2fd33ceb42ea25971ac7f881566ee
MutabPato <patrickmtbz@gmail.com>
2023-09-15 08:31:33 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,16 @@ _putchar('\n');
 }
 else
 {
-int i;
+int i, j;
 for (i = 0; i < n ; i++)
 {
+for (j = n - i ; j < n ; j++)
+{
 _putchar(' ');
-i++;
 }
+_putchar(92);
 _putchar('\n');
+}
 }
 return;
 }
