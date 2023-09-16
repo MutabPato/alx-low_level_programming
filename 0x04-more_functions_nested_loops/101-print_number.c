8fe5e8df819e5f12902f884f137c537af6979f16
MutabPato <patrickmtbz@gmail.com>
2023-09-16 08:48:05 -0700

    Integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,37 +7,14 @@
  */
 void print_number(int n)
 {
-int i;
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
-if (n >= 1000)
+if (n / 10 != 0)
 {
-for (i = 1000 ; i >= 10 ; i /= 10)
-{
-_putchar('0' + n / i);
-n = n % i;
+print_number(n / 10);
 }
 _putchar('0' + n % 10);
-}
-else if (n >= 100)
-{
-for (i = 100 ; i >= 10 ; i /= 10)
-{
-_putchar('0' + n / i);
-n = n % i;
-}
-_putchar('0' + n % 10);
-}
-else if (n >= 10)
-{
-_putchar('0' + n / 10);
-_putchar('0' + n % 10);
-}
-else if (n < 10)
-{
-_putchar('0' + n % 10);
-}
 }
