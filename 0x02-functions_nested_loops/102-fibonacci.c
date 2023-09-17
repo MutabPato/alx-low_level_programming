fcb62a3377ed9485c4b8315303044f5a1f5ff33f
MutabPato <patrickmtbz@gmail.com>
2023-09-17 05:21:56 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main -  prints sum of multiples
+ *
+ *Return: ends program.
+ */
+int  main(void)
+{
+int i;
+long int fib[50];
+fib[0] = 1;
+fib[1] = 2;
+for (i = 2; i < 50 ; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+for (i = 0; i < 50 ; i++)
+{
+printf("%ld", fib[i]);
+if (i < 49)
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
