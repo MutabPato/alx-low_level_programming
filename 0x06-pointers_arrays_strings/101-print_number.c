48cb023b7960e519c2dcb256943804730a46f7f4
MutabPato <patrickmtbz@gmail.com>
2023-09-22 11:56:37 -0700

    Print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,15 @@
  */
 void print_number(int n)
 {
-if (n < 0)
-{
-n = -n;
-_putchar('-');
-}
 if (n == 0)
 {
 _putchar('0');
 return;
+}
+if (n < 0)
+{
+_putchar('-');
+n = -n;
 }
 if (n / 10 != 0)
 {
