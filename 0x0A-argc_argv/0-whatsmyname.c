796a347a1dec904fe6d3d638623e50f6e6b61ec6
MutabPato <patrickmtbz@gmail.com>
2023-10-02 11:23:52 -0700

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0.
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
