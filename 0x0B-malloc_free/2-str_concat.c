21f1c3732c949a282cf57da4369b12802958bf17
MutabPato <patrickmtbz@gmail.com>
2023-10-04 09:06:33 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,14 @@ char *str_concat(char *s1, char *s2)
 {
 char *str;
 int i, len1 = 0, len2 = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 while (s1[len1] != '\0')
 {
 len1++;
@@ -23,14 +31,6 @@ str = malloc(len1 + len2 + 1);
 if (str == NULL)
 {
 return (NULL);
-}
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
 }
 for (i = 0; i < len1; i++)
 {
