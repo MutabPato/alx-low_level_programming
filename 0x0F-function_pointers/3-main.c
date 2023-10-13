2c2e6192b433347b246483b545d14eab20e3847c
MutabPato <patrickmtbz@gmail.com>
2023-10-13 14:42:49 -0700

    computes an operation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print operation
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return result.
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+char *operator;
+int (*operation)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+operator = argv[2];
+operation = get_op_func(operator);
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+result = operation(num1, num2);
+printf("%d\n", result);
+return (0);
+}
