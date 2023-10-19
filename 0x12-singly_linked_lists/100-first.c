c857550b028a63f50aeedef8bc433f035dbab163
MutabPato <patrickmtbz@gmail.com>
2023-10-19 16:20:33 -0700

    First

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+void print(void) __attribute__ ((constructor));
+
+/**
+ * print - prints
+ *
+ * Return: nothing.
+ */
+void print(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
