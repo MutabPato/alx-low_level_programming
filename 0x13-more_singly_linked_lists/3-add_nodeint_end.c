7c970c4bac96741200df017adb606c63586e97e0
MutabPato <patrickmtbz@gmail.com>
2023-10-24 12:21:22 -0700

    add_nodeint_end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(int));
 
 	if (new_node == NULL)
 	{
@@ -31,5 +31,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		temp->next = new_node;
 	}
-	return (new_node);
+	return (*head);
 }
