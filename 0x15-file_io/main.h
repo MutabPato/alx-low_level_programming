6cf36005251845c4b3b395bf9dfd02332bf07a51
MutabPato <patrickmtbz@gmail.com>
2023-10-30 15:19:25 -0700

    ELF

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+int main(int argc, char *argv[]);
+void print_elf_header_info(Elf64_Ehdr *header);
+void display_error(void);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
