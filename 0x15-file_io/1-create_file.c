e6f5d91f710e06bfb299a0d733cf04fd073b4c4a
MutabPato <patrickmtbz@gmail.com>
2023-10-31 09:35:18 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	text_length = strlen(text_content);
-	bytesWritten = write(STDOUT_FILENO, text_content, text_length);
+	bytesWritten = write(file_descriptor, text_content, text_length);
 	if (bytesWritten == -1 || (size_t)bytesWritten != text_length)
 	{
 		close(file_descriptor);
