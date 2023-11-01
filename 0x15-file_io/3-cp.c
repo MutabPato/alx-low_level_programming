0cb1320fa4bf2e5a8a7582c0e2657bb3ce904f60
MutabPato <patrickmtbz@gmail.com>
2023-11-01 14:14:57 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,80 +1,57 @@
 #include "main.h"
 
 /**
+ * error_exit - prints error message and exit
+ * @code: exit code
+ * @message: message to print
+ * Return : nothing.
+ */
+void error_exit(int code, const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
+/**
  * main -  copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 - success.
  */
-int main(int argc , char **argv)
+int main(int argc, char **argv)
 {
 	int fd1, fd2, bytesRead, bytesWritten;
-	char *buffer;
-	char *file_from, *file_to;
+	char buffer[1024], *file_from, *file_to;
 
 	file_from = argv[1];
 	file_to = argv[2];
 
-	if(argc != 3)
-	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
-	}
-
-	if (file_from == NULL)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
-	}
-
-	buffer = malloc(1024);
-	if (buffer == NULL)
-		return (0);
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to");
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+		error_exit(98, "Error: Can't read from file");
 
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		error_exit(99, "Error: Can't write to file");
+
+	do {
 	bytesRead = read(fd1, buffer, 1024);
 	if (bytesRead == -1)
-	{
-		free(buffer);
-		close(fd1);
-		return (0);
-	}
-
-	fd2 = open(file_to, O_CREAT | O_RDWR | O_TRUNC);
-	if (fd2 == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buffer);
-		close (fd2);
-		exit (99);
-	}
+		error_exit(98, "Error: Can't read from file");
 
 	bytesWritten = write(fd2, buffer, bytesRead);
 	if (bytesWritten == -1 || bytesWritten != bytesRead)
-	{
-		free(buffer);
-		close(fd2);
-		return (0);
-	}
+		error_exit(99, "Error: Can't write to file");
+	} while (bytesRead > 0);
 
-	free(buffer);
-	close(fd1);
-	close(fd2);
-	if(fd1 != -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd 1\n");
-		exit (100);
-	}
-	if(fd2 != -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd 2\n");
-		exit (100);
-	}
+	if (close(fd1) == -1)
+		error_exit(100, "Error: Can't close fd 1");
+
+	if (close(fd2) == -1)
+		error_exit(100, "Error: Can't close fd 2");
+
 	return (0);
 }
