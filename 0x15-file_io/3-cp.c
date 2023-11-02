f6cc406201f16ec89ba7ef3ebc0f786aac954d67
MutabPato <patrickmtbz@gmail.com>
2023-11-02 07:19:29 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * error_exit - prints error message and exit
- * @code: exit code
- * @message: message to print
- * Return : nothing.
- */
-void error_exit(int code, const char *message)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
-}
-
-/**
  * main -  copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
@@ -27,31 +15,31 @@ int main(int argc, char **argv)
 	file_to = argv[2];
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to");
+		error_exit0(97, "Usage: cp file_from file_to");
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-		error_exit(98, "Error: Can't read from file");
+		error_exit1(98, "Error: Can't read from file", file_from);
 
 	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
-		error_exit(99, "Error: Can't write to file");
+		error_exit1(99, "Error: Can't write to file", file_to);
 
 	do {
 	bytesRead = read(fd1, buffer, 1024);
 	if (bytesRead == -1)
-		error_exit(98, "Error: Can't read from file");
+		error_exit1(98, "Error: Can't read from file", file_from);
 
 	bytesWritten = write(fd2, buffer, bytesRead);
 	if (bytesWritten == -1 || bytesWritten != bytesRead)
-		error_exit(99, "Error: Can't write to file");
+		error_exit1(99, "Error: Can't write to file", file_to);
 	} while (bytesRead > 0);
 
 	if (close(fd1) == -1)
-		error_exit(100, "Error: Can't close fd 1");
+		error_exit2(100, "Error: Can't close fd", fd1);
 
 	if (close(fd2) == -1)
-		error_exit(100, "Error: Can't close fd 2");
+		error_exit2(100, "Error: Can't close fd", fd2);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,45 @@
 #include <unistd.h>
 #include <elf.h>
 #include <string.h>
+
+/**
+ * error_exit1 - prints error message and exit
+ * @code: exit code
+ * @message: message to print
+ * @filename: filename to print
+ * Return : nothing.
+ */
+void error_exit1(int code, const char *message, const char *filename)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	exit(code);
+}
+
+/**
+ * error_exit2 - prints error message and exit
+ * @code: exit code
+ * @message: message to print
+ * @value: value to print
+ * Return : nothing.
+ */
+void error_exit2(int code, const char *message, const int value)
+{
+	dprintf(STDERR_FILENO, "%s %d\n", message, value);
+	exit(code);
+}
+
+/**
+ * error_exit0 - prints error message and exit
+ * @code: exit code
+ * @message: message to print
+ * Return : nothing.
+ */
+void error_exit0(int code, const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
 int main(int argc, char **argv);
 void print_elf_header_info(Elf64_Ehdr *header);
 void display_error(void);
